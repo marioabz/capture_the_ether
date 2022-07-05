26f9310c2b2ba0cdf4b90ea74e5bdb50baf641f6
mario <mario.briseno7@gmail.com>
2022-07-04 23:47:34 -0500

    Added initialization files to hardhat project

---
+++ token_sale/hardhat.config.js
@@ -0,0 +1,44 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const KEY = process.env.PK
+const ENDPOINT = process.env.ENDPOINT
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.4.22"
+      },
+      {
+        version: "0.8.13"
+      }
+    ]
+  },
+  networks: {
+    ropsten: {
+      url: ENDPOINT,
+      accounts: [KEY],
+    },
+    local: {
+      url: "http://127.0.0.1:8545/",
+      aacounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
+    }
+  }
+};
